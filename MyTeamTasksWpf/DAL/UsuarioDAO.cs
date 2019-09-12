d7b1cdd9a4b20c9572722e437fe4da23e50898fe
Yahgo Batista <Yahgobatista@LIVE.COM>
2019-09-12 00:27:09 -0300

    Adição do material design e configuração e criação do cadastro de Usuario

---
+++ MyTeamTasksWpf/DAL/UsuarioDAO.cs
@@ -0,0 +1,34 @@
+﻿using MyTeamTasksWpf.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTeamTasksWpf.DAL
+{
+    class UsuarioDAO
+    {
+        private static Context ctx = SingletonContext.getInstance();
+
+        public static void CadastrarUsuario(Usuario u) {
+            ctx.Usuarios.Add(u);
+            ctx.SaveChanges();
+            Console.WriteLine(u);
+        }
+
+        public static List<Usuario> ListarUsuarios() => ctx.Usuarios.ToList();
+
+        public static Usuario BuscarUsuarioPorNome(Usuario u) {
+            return ctx.Usuarios.SingleOrDefault(x => x.Nickname.Equals(u.Nickname));
+        }
+
+        public static void RemoverUsuario(Usuario u) {
+
+            //Busca primeiro e depois remove
+            BuscarUsuarioPorNome(u);
+            ctx.Usuarios.Remove(u);
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ MyTeamTasksWpf/View/vUsuario/CriarUsuario.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MyTeamTasksWpf.DAL;
+using MyTeamTasksWpf.Model;
+
+namespace MyTeamTasksWpf.View.vUsuario
+{
+    /// <summary>
+    /// Interaction logic for CriarUsuario.xaml
+    /// </summary>
+    public partial class CriarUsuario : Window
+    {
+        public CriarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInserir_Click(object sender, RoutedEventArgs e)
+        {
+            Usuario u = new Usuario();
+
+            lbMensagemSucesso.Content = "";
+
+
+            if (!txtCargo.Text.Equals("") &&
+                !txtNickName.Text.Equals("") &&
+                !txtSenha.Text.Equals(""))
+            {
+                u.Cargo = txtCargo.Text;
+                u.Nickname = txtNickName.Text;
+                u.Senha = txtSenha.Text;
+
+                UsuarioDAO.CadastrarUsuario(u);
+
+                lbMensagemSucesso.Content = "Usuario inserido !";
+            }
+            else
+            {
+                MessageBox.Show("Preencha todos os campos antes de inserir !", 
+                    "Campos vazios encontrados", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+    }
+}
