9192cabe7eb3d8ebc1c2f660fb8710d3e8d8be20
Yahgo Batista <Yahgobatista@LIVE.COM>
2019-09-22 17:36:25 -0300

    Criacao da tela de login

---
+++ MyTeamTasksWpf/View/vLogin/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MyTeamTasksWpf.View.vDashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyTeamTasksWpf.View.vLogin
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            this.Close();
+        }
+    }
+}
